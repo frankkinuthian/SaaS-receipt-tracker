969e6c130779ddfd1347e211ef6392c817581fd7
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-06-29 12:15:10 +0300

    Update dependencies, enhance global styles, and restructure layout. Added new components for improved UI and integrated Schematic for user tracking.

---
+++ components/Header.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React from "react";
+import { Shield } from "lucide-react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+
+function Header() {
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
+
+  return (
+    <div
+      className={`p-4 flex justify-between items-center ${
+        isHomePage ? "bg-blue-50" : "bg-white borger-b borg=der blue-50"
+      }`}
+    >
+      <Link href="/" className="flex items-center">
+        <Shield className="w-6 h-6 text-blue-600 mr-2" />
+        <h1 className="text-xl font-semibold">Expensio</h1>
+      </Link>
+
+      <div className="flex items-center space-x-4">
+        <SignedIn>
+          <Link href="/receipts">
+            <Button variant="outline">My receipts</Button>
+          </Link>
+
+          <Link href="/manage-plan">
+            <Button>Manage Plan</Button>
+          </Link>
+
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button>Sign in</Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ components/schematic/SchematicEmbed.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import {
+  SchematicEmbed as SchematicEmbedComponent,
+  EmbedProvider,
+} from "@schematichq/schematic-components";
+
+function SchematicEmbed({
+  accessToken,
+  componentId,
+}: {
+  accessToken: string;
+  componentId: string;
+}) {
+  return (
+    <EmbedProvider>
+      <SchematicEmbedComponent accessToken={accessToken} id={componentId} />
+    </EmbedProvider>
+  );
+}
+
+export default SchematicEmbed;
